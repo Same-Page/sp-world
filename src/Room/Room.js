f5ef5761375be58f85820698c78aab6f229fed34
Archi-tech <swotong@umich.edu>
2020-08-14 02:25:41 -0700

    openvidu

--- src/Room/Room.js
+++ src/Room/Room.js
@@ -2,6 +2,9 @@ import "./Room.css"
 import React, { useState, useEffect } from "react"
 
 import { Form, Input, Upload, message, Modal, Button } from "antd"
+
+import VideoRoomComponent from "../Video/VideoRoomComponent"
+
 const formItemLayout = {
 	labelCol: {
 		xs: {
@@ -53,6 +56,7 @@ function Room() {
 		<Modal
 			// mask={false}
 			// maskClosable={false}
+			width={roomData ? 720 : 500}
 			footer={null}
 			title={title}
 			visible={showRoomInfo}
@@ -61,6 +65,7 @@ function Room() {
 				setShowRoomInfo(false)
 			}}
 		>
+			{roomData && <VideoRoomComponent />}
 			{!roomData && (
 				<Form
 					// className="sp-form"
@@ -75,9 +80,12 @@ function Room() {
 					// }}
 					scrollToFirstError
 				>
+					<p>
+						看来你发现了一个还没有主题的房间呢！先来设置一下该房间的主题吧，比如一起看某部电视剧，某部电影，还是聊一聊某个具体话题呢？
+					</p>
 					<Form.Item
 						name="name"
-						label="名称"
+						label="房间名称（必填）"
 						hasFeedback
 						rules={[
 							{
@@ -90,7 +98,7 @@ function Room() {
 					</Form.Item>
 					<Form.Item
 						name="about"
-						label="介绍"
+						label="房间介绍"
 						hasFeedback
 						rules={[
 							{
